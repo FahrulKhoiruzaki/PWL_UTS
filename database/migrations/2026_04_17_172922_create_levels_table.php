2a0b00114bc5fe5919e67d0e6cd89d7e0ff0b5dd
fahrulKhoiruzaki <244107020234@student.polinema.ac.id>
2026-04-18 02:15:48 +0700

    perbaikan

--- database/migrations/2026_04_17_172922_create_levels_table.php
+++ database/migrations/2026_04_17_172922_create_levels_table.php
@@ -12,12 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('levels', function (Blueprint $table) {
-            Schema::create('levels', function (Blueprint $table){
-                $table->id('level_id');
-                $table->string('level_kode', 10)->unique();
-                $table->string('level_nama', 100);
-                $table->timestamps();
-            });
+            $table->id('level_id');
+            $table->string('level_kode', 10)->unique();
+            $table->string('level_nama', 100);
+            $table->timestamps();
         });
     }
 
