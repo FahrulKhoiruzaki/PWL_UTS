53265e52d9a59cf997d5d2c09cc4d87c161746ff
fahrulKhoiruzaki <244107020234@student.polinema.ac.id>
2026-04-18 22:46:40 +0700

    menambahkan resource FIlament data level

---
+++ app/Filament/Resources/Levels/Tables/LevelsTable.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Filament\Resources\Levels\Tables;
+
+use Dom\Text;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Table;
+use Filament\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
+
+class LevelsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('level_kode')
+                    ->label('Kode Level')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('level_nama')
+                    ->label('Nama Level')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime('d-m-Y H:i')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                DeleteBulkAction::make(),
+            ]);
+    }
+}
